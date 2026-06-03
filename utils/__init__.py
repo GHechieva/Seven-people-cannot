2beb4e8d69707673ff867a60fdcb3c8c8935f5fb
GHechieva <92372712+GHechieva@users.noreply.github.com>
2026-06-03 12:07:49 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,4 @@ from .states import (
     TripCreateStates, TripJoinStates, ExpenseStates,
     EditExpenseStates, ReceiptStates, NotificationStates,
 )
-from .formatting import fmt_amount, fmt_expense_line, fmt_balance_line, fmt_trip_header
+from .formatting import fmt_amount, fmt_expense_line, fmt_trip_header
